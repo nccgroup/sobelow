5d7790e697ce18bbaeba0348ac03ff37a44b5cf1
GriffinMB <byatt.griffin@gmail.com>
2017-04-09 21:14:37 -0400

    Add secure transport checks

--- lib/sobelow/config.ex
+++ lib/sobelow/config.ex
@@ -1,30 +1,32 @@
 defmodule Sobelow.Config do
   alias Sobelow.Utils
+  @prod_path "config/prod.exs"
+  @prod_secret_path "config/prod.secret.exs"
 
   def hardcoded_secrets do
-    prod_path = "config/prod.exs"
-    prod_secret_path = "config/prod.secret.exs"
-
     IO.puts IO.ANSI.cyan_background() <>
       IO.ANSI.black() <>
       "Searching for Hardcoded Secrets" <>
       IO.ANSI.reset()
     IO.puts "\n-----------------------------------------------\n"
 
-    get_secrets_by_file(:secret_key_base, prod_path)
-    |> enumerate_secrets(prod_path)
+    get_configs_by_file(:secret_key_base, @prod_path)
+    |> enumerate_secrets(@prod_path)
 
-    get_secrets_by_file(:secret_key_base, prod_secret_path)
-    |> enumerate_secrets(prod_secret_path)
+    get_configs_by_file(:secret_key_base, @prod_secret_path)
+    |> enumerate_secrets(@prod_secret_path)
 
-    get_secrets_by_file(:password, prod_path)
-    |> enumerate_secrets(prod_path)
+    get_configs_by_file(:password, @prod_path)
+    |> enumerate_secrets(@prod_path)
 
-    get_secrets_by_file(:password, prod_secret_path)
-    |> enumerate_secrets(prod_secret_path)
+    get_configs_by_file(:password, @prod_secret_path)
+    |> enumerate_secrets(@prod_secret_path)
+
+    get_configs_by_file(:https, @prod_path)
+    |> handle_https
   end
 
-  defp get_secrets_by_file(secret, file) do
+  defp get_configs_by_file(secret, file) do
     if File.exists?(file) do
       Utils.get_configs(secret, file)
     else
@@ -38,6 +40,26 @@ defmodule Sobelow.Config do
         print_finding(file, lineno, val)
       end
     end
+  end
+
+  defp handle_https(opts) do
+    if length(opts) === 0 do
+      print_finding(:https)
+    else
+      if length(Utils.get_configs(:force_ssl, @prod_path)) === 0 do
+        print_finding(:hsts)
+      end
+    end
+  end
+
+  defp print_finding(:https) do
+    IO.puts IO.ANSI.red() <> "HTTPS Not Enabled - High Confidence" <> IO.ANSI.reset()
+    IO.puts "\n-----------------------------------------------\n"
+  end
+
+  defp print_finding(:hsts) do
+    IO.puts IO.ANSI.yellow() <> "HSTS Not Enabled - Medium Confidence" <> IO.ANSI.reset()
+    IO.puts "\n-----------------------------------------------\n"
   end
 
   defp print_finding(file, line_no, val) do
