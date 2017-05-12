a36fbd60def67ce6089dd1179faada95acf73732
Griffin Byatt <byatt.griffin@gmail.com>
2017-05-12 14:03:55 -0400

    Version bump

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Sobelow.Mixfile do
 
   def project do
     [app: :sobelow,
-     version: "0.1.0",
+     version: "0.2.0",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
