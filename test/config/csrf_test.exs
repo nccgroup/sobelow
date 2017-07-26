cfde3b2553299ff0ef5e9894fe4085d752a9a786
Griffin Byatt <byatt.griffin@gmail.com>
2017-07-26 10:40:26 -0400

    Update test to handle new pipeline check

--- test/config/csrf_test.exs
+++ test/config/csrf_test.exs
@@ -5,24 +5,24 @@ defmodule Sobelow.Config.CSRFTest do
   test "checks normal router" do
   	router = "./test/fixtures/good_router.ex"
     refute Utils.get_pipelines(router)
-					 |> Enum.any?(&Utils.is_vuln_pipeline/1)
+					 |> Enum.any?(&Utils.is_vuln_pipeline(&1, :csrf))
   end
 
   test "checks normal router with named session key" do
   	router = "./test/fixtures/good_router_with_session_key.ex"
     refute Utils.get_pipelines(router)
-					 |> Enum.any?(&Utils.is_vuln_pipeline/1)
+					 |> Enum.any?(&Utils.is_vuln_pipeline(&1, :csrf))
   end
 
   test "checks normal router with with" do
     router = "./test/fixtures/good_router_with_with.ex"
     refute Utils.get_pipelines(router)
-           |> Enum.any?(&Utils.is_vuln_pipeline/1)
+           |> Enum.any?(&Utils.is_vuln_pipeline(&1, :csrf))
   end
 
   test "checks bad router" do
   	router = "./test/fixtures/bad_router.ex"
     assert Utils.get_pipelines(router)
-					 |> Enum.any?(&Utils.is_vuln_pipeline/1)
+					 |> Enum.any?(&Utils.is_vuln_pipeline(&1, :csrf))
   end
 end
