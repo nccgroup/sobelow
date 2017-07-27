ded1f21c3129ba093e29e0970ed39aa8afc51600
Griffin Byatt <byatt.griffin@gmail.com>
2017-07-27 09:04:36 -0400

    Add secure headers documentation

--- lib/sobelow/config/headers.ex
+++ lib/sobelow/config/headers.ex
@@ -1,6 +1,19 @@
 defmodule Sobelow.Config.Headers do
   @moduledoc """
+  # Missing Secure HTTP Headers
 
+  By default, Phoenix HTTP responses contain a number of
+  secure HTTP headers that attempt to mitigate XSS,
+  click-jacking, and content-sniffing attacks.
+
+  Missing Secure HTTP Headers is flagged by `sobelow` when
+  a pipeline accepts "html" requests, but does not implement
+  the `:put_secure_browser_headers` plug.
+
+  Secure Headers checks can be ignored with the following
+  command:
+
+      $ mix sobelow -i Config.Headers
   """
   alias Sobelow.Utils
   use Sobelow.Finding
