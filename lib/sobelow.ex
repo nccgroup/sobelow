453de1b59bf40c9a39dfca7690e172351b189f31
GriffinMB <byatt.griffin@gmail.com>
2017-09-19 07:26:51 -0400

    Update phx-rc note

--- lib/sobelow.ex
+++ lib/sobelow.ex
@@ -18,14 +18,8 @@ defmodule Sobelow do
   alias Sobelow.Vuln
   alias Sobelow.FindingLog
   alias Mix.Shell.IO, as: MixIO
-  # In order to support the old application structure, as well as the
-  # upcoming application structure (ie all in lib directory, vs pulled
-  # into a web directory), there are a number of different "roots" in
-  # use.
-  #
-  # As of Phoenix-rc.3, this now supports 3+ directory structures.
-  # Support for the original rc structure should be deprecated after
-  # Phoenix 1.3
+  # Remove directory structure check for release candidate
+  # prior to 1.0
   def run() do
     project_root = get_env(:root) <> "/"
     if !get_env(:private), do: version_check(project_root)
