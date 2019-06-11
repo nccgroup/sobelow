d548bc996600f7a31ccbd8d9182658703e9f5b8f
GriffinMB <byatt.griffin@gmail.com>
2019-06-10 22:02:35 -0500

    Update ex_doc

--- mix.exs
+++ mix.exs
@@ -36,7 +36,7 @@ defmodule Sobelow.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:ex_doc, "~> 0.14", only: :dev}]
+    [{:ex_doc, "~> 0.20", only: :dev}]
   end
 
   defp package() do
