4761a6e4112085593abc43cd589e067d86628726
Adriano Mitre <adriano.mitre@gmail.com>
2020-08-13 13:19:49 -0300

    add "mix format --check-formatted" to CI

--- test/fixtures/cswh/good_endpoint_2.ex
+++ test/fixtures/cswh/good_endpoint_2.ex
@@ -1,5 +1,8 @@
 defmodule PhoenixWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :phoenix
 
-  socket("/socket", PhoenixInternalsWeb.UserSocket, websocket: [check_origin: true], longpoll: false)
+  socket("/socket", PhoenixInternalsWeb.UserSocket,
+    websocket: [check_origin: true],
+    longpoll: false
+  )
 end
