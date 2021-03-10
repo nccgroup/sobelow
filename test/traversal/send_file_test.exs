af47d169d9e8a065ff531d64ab98786ae10fa3f4
Daniel Paul Searles <daniel.paul.searles@gmail.com>
2021-03-09 21:20:34 -0700

    Add test coverage for Sobelow.Traversal.SendFile

---
+++ test/traversal/send_file_test.exs
@@ -0,0 +1,53 @@
+defmodule SobelowTest.Traversal.SendFile do
+  use ExUnit.Case
+  import Sobelow, only: [is_vuln?: 1]
+  alias Sobelow.Traversal.SendFile
+
+  test "vulnerable send_file" do
+    func = """
+    def index(conn, %{"test" => test}) do
+      send_file(conn, 200, test)
+    end
+    """
+
+    {_, ast} = Code.string_to_quoted(func)
+
+    assert SendFile.parse_def(ast) |> is_vuln?
+  end
+
+  test "vulnerable aliased send_file" do
+    func = """
+    def index(conn, %{"test" => test}) do
+      Plug.Conn.send_file(conn, 200, test)
+    end
+    """
+
+    {_, ast} = Code.string_to_quoted(func)
+
+    assert SendFile.parse_def(ast) |> is_vuln?
+  end
+
+  test "vulnerable indirect aliased send_file" do
+    func = """
+    def index(conn, %{"test" => test}) do
+      Plug.Conn.send_file(conn, 200, Path.expand(test))
+    end
+    """
+
+    {_, ast} = Code.string_to_quoted(func)
+
+    assert SendFile.parse_def(ast) |> is_vuln?
+  end
+
+  test "safe send_file" do
+    func = """
+    def index(conn, %{"test" => _test}) do
+      send_file(conn, 200, "file.txt")
+    end
+    """
+
+    {_, ast} = Code.string_to_quoted(func)
+
+    refute SendFile.parse_def(ast) |> is_vuln?
+  end
+end
