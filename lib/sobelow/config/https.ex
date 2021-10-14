7c08f1e345150c5041959eba6cd3d7ba210766eb
Kian-Meng, Ang <kianmeng@cpan.org>
2021-10-14 23:05:13 +0800

    Fix typo

--- lib/sobelow/config/https.ex
+++ lib/sobelow/config/https.ex
@@ -2,7 +2,7 @@ defmodule Sobelow.Config.HTTPS do
   @moduledoc """
   # HTTPS
 
-  Without HTTPS, attackers in a priveleged network position can
+  Without HTTPS, attackers in a privileged network position can
   intercept and modify traffic.
 
   Sobelow detects missing HTTPS by checking the prod
