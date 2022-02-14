92277c58e63deaf87b69ae8f2c406c518416be10
Juan C Galvis <8420868+juancgalvis@users.noreply.github.com>
2022-02-14 09:29:50 -0500

    fix sonarqube text range when file issue

--- lib/sobelow/formatter/sonar_qube.ex
+++ lib/sobelow/formatter/sonar_qube.ex
@@ -4,7 +4,7 @@ defmodule Sobelow.Formatter.SonarQube do
   @behaviour Formatter
 
   @impl Formatter
-  def format_findings(%{high: highs, medium: meds, low: lows} = all, vsn) do
+  def format_findings(%{high: highs, medium: meds, low: lows}, vsn) do
     prefix = Application.get_env(:sobelow, :sonarqube_base_folder, "")
     issues = Enum.map(highs, fn {_type, finding} -> format_finding(finding, vsn, prefix) end)
              |> Enum.concat(Enum.map(meds, fn {_type, finding} -> format_finding(finding, vsn, prefix) end))
@@ -15,20 +15,28 @@ defmodule Sobelow.Formatter.SonarQube do
   defp format_finding(%Finding{} = finding, vsn, prefix) do
     [modId, _] = String.split(finding.type, ":", parts: 2)
     rule = Sobelow.get_mod(modId)
+
+    location = %{
+      filePath: "#{prefix}#{finding.filename}",
+      message: "#{finding.type} #{finding.vuln_variable} \n Help: #{rule.details()}"
+    }
+
+    location = with_text_range(location, finding)
+
     %{
       ruleId: rule.id(),
       severity: confidence_to_severity(finding.confidence),
       type: "VULNERABILITY",
       engineId: "sobelow-#{vsn}",
-      primaryLocation: %{
-        filePath: "#{prefix}#{finding.filename}",
-        message: "#{finding.type} #{finding.vuln_variable} \n Help: #{rule.details()}",
-        textRange: %{
-          startLine: finding.vuln_line_no
-        }
-      }
+      primaryLocation: location
     }
   end
+
+  defp with_text_range(%{} = location, %Finding{vuln_line_no: line}) when line > 0 do
+    Map.put(location, :textRange, %{startLine: line})
+  end
+
+  defp with_text_range(%{} = location, %Finding{}), do: location
 
   defp confidence_to_severity(:high), do: "CRITICAL"
   defp confidence_to_severity(:medium), do: "MAJOR"
