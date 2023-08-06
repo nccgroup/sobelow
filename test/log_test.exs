38a9ad0821f4eba66ee478d106b8bbedebd0d4c9
Holden Oullette <6202965+houllette@users.noreply.github.com>
2023-08-06 10:23:25 -0600

    added mix format changes

--- test/log_test.exs
+++ test/log_test.exs
@@ -4,24 +4,25 @@ defmodule SobelowTest.LogTest do
 
   # log_json_finding(line_no, filename, fun_name, var, severity, type)
   test "Log JSON finding with function as function name" do
-    output = Jason.decode!("""
-    {
-      "findings": {
-        "high_confidence": [
-          {
-            "file": "file.ex",
-            "line": 1,
-            "type": "N/A",
-            "variable": "var"
-          }
-        ],
-        "low_confidence": [],
-        "medium_confidence": []
-      },
-      "sobelow_version": "1.0.0",
-      "total_findings": 1
-    }
-    """)
+    output =
+      Jason.decode!("""
+      {
+        "findings": {
+          "high_confidence": [
+            {
+              "file": "file.ex",
+              "line": 1,
+              "type": "N/A",
+              "variable": "var"
+            }
+          ],
+          "low_confidence": [],
+          "medium_confidence": []
+        },
+        "sobelow_version": "1.0.0",
+        "total_findings": 1
+      }
+      """)
 
     FindingLog.start_link()
     Fingerprint.start_link()
